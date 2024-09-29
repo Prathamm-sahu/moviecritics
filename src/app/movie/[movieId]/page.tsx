ecbbee4416000e9b1fc77423ac78cb0585d28c2a
Pratham Sahu <prathamsahu051@gmail.com>
2024-09-29 23:21:26 +0530

    Created movie reviews route

--- src/app/movie/[movieId]/page.tsx
+++ src/app/movie/[movieId]/page.tsx
@@ -1,35 +1,97 @@
-import { Button } from '@/components/ui/button'
-import { SquarePen, Trash, Trash2 } from 'lucide-react'
-import { FC } from 'react'
+"use client";
+
+import DeleteModal from "@/components/DeleteModal";
+import { Button } from "@/components/ui/button";
+import { useToast } from "@/hooks/use-toast";
+import { Movie, MovieReview } from "@prisma/client";
+import axios from "axios";
+import { Loader, SquarePen, Trash, Trash2 } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { FC, useEffect, useState } from "react";
 
 interface PageProps {
-  
+  params: {
+    movieId: string;
+  };
 }
 
-const Page: FC<PageProps> = ({}) => {
+type ExtendedMovie = Movie & {
+  movieReviews: MovieReview[];
+};
+
+const Page: FC<PageProps> = ({ params }) => {
+  const [movie, setMovie] = useState<ExtendedMovie>();
+  const router = useRouter()
+  const { toast } = useToast()
+  const fetchMovieById = async () => {
+    try {
+      const { data } = await axios.get(
+        `http://localhost:3000/api/movie/${params.movieId}`
+      );
+      setMovie(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log(movie);
+
+  useEffect(() => {
+    fetchMovieById();
+  }, [movie]);
+
+  const handleDelete = async (id: string) => {
+    await axios.delete(`/api/review/${id}`)
+    fetchMovieById()
+    router.refresh()
+    toast({
+      title: "Movie deleted successfully"
+    })
+  }
+
+  if (!movie) {
+    return (
+      <div className="animate-spin flex justify-center items-center">
+        <Loader />
+      </div>
+    );
+  }
+
+
   return (
     <div className="min-h-screen">
       <main className="">
         <div className="bg-white rounded-lg p-6">
           <div className="flex justify-between items-center mb-6">
-            <h2 className="text-xl md:text-4xl font-medium text-gray-800">Star Wars: A New Hope</h2>
-            <span className=" text-xl md:text-4xl font-bold text-indigo-600">8.33/10</span>
+            <h2 className="text-xl md:text-4xl font-medium text-gray-800">
+              {movie?.name}
+            </h2>
+            {movie?.averageRating && <span className=" text-xl md:text-4xl font-bold text-indigo-600">{`${movie?.averageRating}/10`}</span>}
           </div>
-          {[9, 8, 8].map((rating, index) => (
-            <div key={index} className="border-[3px] border-gray-400 p-4 mb-4 space-y-4">
+          {movie?.movieReviews.map(({ id, rating, username }) => (
+            <div
+              key={id}
+              className="border-[3px] border-gray-400 p-4 mb-4 space-y-4"
+            >
               <div className="flex justify-between items-start">
-                <p className="text-lg text-gray-900 mb-2">This is the best movie ever! I really enjoyed it.</p>
-                <span className="text-xl font-bold text-indigo-600">{rating}/10</span>
+                <p className="text-lg text-gray-900 mb-2">
+                  This is the best movie ever! I really enjoyed it.
+                </p>
+                <span className="text-xl font-bold text-indigo-600">
+                  {rating}/10
+                </span>
               </div>
               <div className="flex justify-between items-center ">
-                <span className='italic font-medium'>By Amitav Khandelwal</span>
+                <span className="italic font-medium">By {`${username}`}</span>
                 <div className="space-x-2">
                   <button className="hover:text-indigo-600">
                     <SquarePen className="w-5 h-5 text-gray-600 " />
                   </button>
-                  <button className="hover:text-indigo-600">
-                    <Trash className="w-5 h-5 text-gray-600" />
-                  </button>
+                  <DeleteModal ratingId={id} handleDelete={handleDelete} item="review">
+                    <button className="hover:text-indigo-600">
+                      <Trash className="w-5 h-5 text-gray-600" />
+                    </button>
+                  </DeleteModal>
                 </div>
               </div>
             </div>
@@ -37,7 +99,7 @@ const Page: FC<PageProps> = ({}) => {
         </div>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
--- src/components/NavButton.tsx
+++ src/components/NavButton.tsx
@@ -1,19 +1,28 @@
+"use client"
+
 import { FC } from "react";
 import { Button } from "./ui/button";
+import AddMovieDialog from "./AddMovieDialog";
+import AddReviewDialog from "./AddReviewDialog";
+import { LogOutIcon } from "lucide-react";
+import { signOut } from "next-auth/react";
 
 interface NavButtonProps {}
 
 const NavButton: FC<NavButtonProps> = ({}) => {
   return (
-    <div className="space-x-2">
+    <div className="space-x-2 flex">
+      <AddMovieDialog />
+      <AddReviewDialog />
       <Button
-        variant="outline"
-        className="bg-white text-indigo-600 hover:bg-indigo-50"
+        className="bg-white text-indigo-600 hover:bg-indigo-50 rounded border-2 border-indigo-300"
+        onClick={() => {
+          signOut({
+            callbackUrl: `${window.location.origin}/sign-in`,
+          });
+        }}
       >
-        Add new movie
-      </Button>
-      <Button className="bg-indigo-600 text-white hover:bg-indigo-700">
-        Add new review
+        <LogOutIcon className="h-4 w-4" />
       </Button>
     </div>
   );
