3d5e068a38ed8e91c65366e663020042f6cd7721
Pratham Sahu <prathamsahu051@gmail.com>
2024-09-29 23:19:37 +0530

    Updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
+import MovieCard from "@/components/MovieCard";
+import SearchBar from "@/components/SearchBar";
+import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Search, SquarePen, Trash } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      Hello Saas Monk
+    <div className="space-y-7">
+      <h2 className="text-4xl">The best movie reviews site!</h2>
+      <div className="mb-6 relative">
+        <SearchBar />
+      </div>
+
+      <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
+        <MovieCard />
+      </div>
     </div>
   );
 }
