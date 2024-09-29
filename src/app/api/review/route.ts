f43d95b08e99bc53356f77e82bbd9899c50191ed
Pratham Sahu <prathamsahu051@gmail.com>
2024-09-29 23:18:08 +0530

    Added a username field

--- src/app/api/review/route.ts
+++ src/app/api/review/route.ts
@@ -12,14 +12,43 @@ export async function POST(req: NextRequest) {
       return new Response("Unauthenticated", { status: 401 })
     }
 
-    const { movieId, rating, comment } = await req.json()
+    const { movieId, rating, comment, username } = await req.json()
 
     await db.movieReview.create({
       data: {
         movieId,
         rating,
         comment,
+        username,
         userId: session.user.id
+      }
+    })
+
+    const movie = await db.movie.findFirst({
+      where: {
+        id: movieId
+      },
+      include: {
+        movieReviews: true
+      }
+    })
+
+    if(!movie) {
+      return new Response("Movie", { status: 404 })
+    }
+  
+    const sumOfRatings = movie?.movieReviews.reduce((total, review) => {
+      return total + review.rating
+    }, 0)
+
+    const avgRating = sumOfRatings/movie?.movieReviews.length
+
+    await db.movie.update({
+      where: {
+        id: movieId
+      },
+      data: {
+        averageRating: avgRating
       }
     })
 
